d2c33f3e40ae7d377e049b3bded44a0c526dd575
mrminh <khongvanminhpfiev@gmail.com>
2020-09-27 20:45:22 +0700

    delete unecessary files

--- public/js/star-input.js
+++
@@ -1,6 +0,0 @@
-function normalText(id, selected) {
-    document.getElementById(id).innerHTML = "Đánh giá";
-}
-function changeText(id, text) {
-    document.getElementById(id).innerHTML = text;
-}
