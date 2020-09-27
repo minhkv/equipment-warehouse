c829715d767fc6fb7c237e0860d39894da141863
mrminh <khongvanminhpfiev@gmail.com>
2020-09-27 16:12:12 +0700

    navbar order dropdown

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -13,18 +13,29 @@
                 <li class="nav-item ">
                     <a class="nav-link {{Route::currentRouteName() == 'home' ? 'active' : ''}}" href="{{route('home')}}">Trang chủ</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link {{Route::currentRouteName() == 'order.index' ? 'active' : ''}}" href="{{route('order.index')}}">Đơn mượn
-                        @if($notification > 0)
-                        <span class="badge badge-warning">{{ $notification }}</span>
-                        @endif
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>Đơn mượn
+                    @if($notification > 0)
+                    <span class="badge badge-warning">{{ $notification }}</span>
+                    @endif
                     </a>
+                    <div class="dropdown-menu dropdown-menu-left" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('order.index') }}">
+                            Danh sách đơn
+                        </a>
+                        <a class="dropdown-item" href="{{ route('order.create') }}">
+                            Tạo đơn
+                        </a>
+                    </div>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>Thiết bị</a>
                     <div class="dropdown-menu dropdown-menu-left" aria-labelledby="navbarDropdown">
                         <a class="dropdown-item" href="{{ route('equipment-template.index') }}">
                             Danh sách thiết bị
+                        </a>
+                        <a class="dropdown-item" href="{{ route('equipment-template.create') }}">
+                            Thêm thiết bị
                         </a>
                         <a class="dropdown-item" href="{{ route('equipment-template.lost') }}">
                             Thiết bị thất lạc
